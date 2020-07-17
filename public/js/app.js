1acf824bba5b343607bf526061d2d2007c881725
Gajendra Singh Pawar <gajendra@bitcot.com>
2020-07-17 09:44:34 +0530

    dasda

--- public/js/app.js
+++ public/js/app.js
@@ -53090,8 +53090,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/laravel/recipe/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/laravel/recipe/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/laravel/laravel-vuejs-recipe/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/laravel/laravel-vuejs-recipe/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
