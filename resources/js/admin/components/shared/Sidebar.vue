9e9db8d0cecd6fbce9a33c978fd274d34a153999
Gajendra Singh Pawar <gajendra@bitcot.com>
2020-08-07 17:20:29 +0530

    admin router link active class added in sidebar menu

--- resources/js/admin/components/shared/Sidebar.vue
+++ resources/js/admin/components/shared/Sidebar.vue
@@ -1,9 +1,13 @@
 <template>
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <router-link to="/dshboard" class="brand-link">
-      <img  v-lazy="userProfile.photo_url" alt="Admin" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
+      <img
+        v-lazy="userProfile.photo_url"
+        alt="Admin"
+        class="brand-image img-circle elevation-3"
+        style="opacity: .8"
+      />
       <span class="brand-text font-weight-light">Recipe Dashboard</span>
     </router-link>
 
@@ -15,115 +19,159 @@
           <!-- <img 
            v-lazy="userProfile.photo_url"
            class="img-circle elevation-2" 
-           alt="User Image"> -->
+          alt="User Image">-->
         </div>
         <div class="info">
-          <router-link to="/update-profile" title="Edit Profile" class="d-block">
-               {{ (userProfile.display_name)?userProfile.display_name:'User Name' }}
-            </router-link>
-            <span style="color:#fff;">
-               ( {{ (userProfile.role)?userProfile.role:' ROLE ' }} }
-            </span>
+          <router-link
+            to="/update-profile"
+            title="Edit Profile"
+            class="d-block"
+          >{{ (userProfile.display_name)?userProfile.display_name:'User Name' }}</router-link>
+          <span style="color:#fff;">( {{ (userProfile.role)?userProfile.role:' ROLE ' }} }</span>
         </div>
-      </div> 
+      </div>
       <!-- Sidebar Menu -->
       <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-           
+        <ul
+          class="nav nav-pills nav-sidebar flex-column"
+          data-widget="treeview"
+          role="menu"
+          data-accordion="false"
+        >
           <li class="nav-item">
             <router-link to="/dashboard" title="View Dashboard" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>Dashboard</p>
             </router-link>
-          </li> 
-
+          </li>
           <li class="nav-item">
-            <router-link to="/clients" title="View Clients List" class="nav-link">
+            <router-link
+              to="/clients"
+              :class="( (this.$route.name == 'clients-list') || (this.$route.name == 'add-clients') || (this.$route.name == 'edit-clients') )?'active':''"
+              title="View Clients List"
+              class="nav-link"
+            >
               <i class="nav-icon fas fa-users"></i>
               <p>Clients List</p>
             </router-link>
-          </li> 
+          </li>
 
-           <li class="nav-item">
-            <router-link to="/authors" title="View Users List" class="nav-link">
+          <li class="nav-item">
+            <router-link
+              to="/authors"
+              title="View Users List"
+              class="nav-link"
+              :class="( (this.$route.name == 'authors-list') || (this.$route.name == 'add-authors') || (this.$route.name == 'edit-authors') )?'active':''"
+            >
               <i class="nav-icon fas fa-user-secret"></i>
               <p>Authors List</p>
             </router-link>
-          </li>  
+          </li>
 
-           <li class="nav-item">
-              <router-link to="/recipes" title="View Recipes List" class="nav-link">
-                <i class="nav-icon fas fa-hamburger"></i>
-                <p>Recipe List</p>
-              </router-link>
-           </li> 
+          <li class="nav-item">
+            <router-link
+              :class="( (this.$route.name == 'recipes-list') || (this.$route.name == 'add-recipe') || (this.$route.name == 'edit-recipe') )?'active':''"
+              to="/recipes"
+              title="View Recipes List"
+              class="nav-link"
+            >
+              <i class="nav-icon fas fa-hamburger"></i>
+              <p>Recipe List</p>
+            </router-link>
+          </li>
 
-           <li class="nav-item">
-            <router-link to="/categories" title="View Recipes Categories " class="nav-link">
+          <li class="nav-item">
+            <router-link
+              :class="( (this.$route.name == 'categories-list') || (this.$route.name == 'add-category') || (this.$route.name == 'edit-category') )?'active':''"
+              to="/categories"
+              title="View Recipes Categories"
+              class="nav-link"
+            >
               <i class="nav-icon fas fa-list"></i>
               <p>Recipe Categories</p>
             </router-link>
-           </li> 
+          </li>
 
-           <li class="nav-item">
-            <router-link to="/tags" title="View Recipe Tags" class="nav-link">
+          <li class="nav-item">
+            <router-link
+              :class="( (this.$route.name == 'tags-list') || (this.$route.name == 'add-tag') || (this.$route.name == 'edit-tag') )?'active':''"
+              to="/tags"
+              title="View Recipe Tags"
+              class="nav-link"
+            >
               <i class="nav-icon fas fa-tags"></i>
               <p>Recipe Tags</p>
             </router-link>
-           </li> 
+          </li>
 
-           <li class="nav-item">
-            <router-link to="/blogs" title="View Blogs List" class="nav-link">
+          <li class="nav-item">
+            <router-link
+              :class="( (this.$route.name == 'blogs-list') || (this.$route.name == 'add-blog') || (this.$route.name == 'edit-blog') )?'active':''"
+              to="/blogs"
+              title="View Blogs List"
+              class="nav-link"
+            >
               <i class="nav-icon fas fa-blog"></i>
               <p>Blogs List</p>
             </router-link>
-           </li> 
+          </li>
 
-           <li class="nav-item">
-            <router-link to="/blog-categories" title="View Blog Tags" class="nav-link">
+          <li class="nav-item">
+            <router-link
+              :class="( (this.$route.name == 'blog-categories-list') || (this.$route.name == 'add-blog-category') || (this.$route.name == 'edit-blog-category') )?'active':''"
+              to="/blog-categories"
+              title="View Blog Tags"
+              class="nav-link"
+            >
               <i class="nav-icon fas fa-list"></i>
-              <p>Blog  Categories</p>
+              <p>Blog Categories</p>
             </router-link>
-           </li> 
+          </li>
 
-           <li class="nav-item">
-            <router-link to="/blog-tags" title="View Blog Tags" class="nav-link">
+          <li class="nav-item">
+            <router-link
+              :class="( (this.$route.name == 'blog-tags-list') || (this.$route.name == 'add-blog-tag') || (this.$route.name == 'edit-blog-tag') )?'active':''"
+              to="/blog-tags"
+              title="View Blog Tags"
+              class="nav-link"
+            >
               <i class="nav-icon fas fa-tags"></i>
               <p>Blog Tags</p>
             </router-link>
-           </li> 
+          </li>
 
-           <li class="nav-item">
+          <li class="nav-item">
             <router-link to="/products" title="View Products List" class="nav-link">
               <i class="nav-icon fab fa-product-hunt"></i>
               <p>Prodcuts List</p>
             </router-link>
-           </li> 
+          </li>
 
           <li class="nav-item">
             <router-link to="/change-password" title="Change Your Password" class="nav-link">
               <i class="nav-icon fas fa-key"></i>
               <p>Change Password</p>
             </router-link>
-          </li>   
+          </li>
 
           <li class="nav-item">
             <router-link to="/update-profile" title="Update Your Profile" class="nav-link">
               <i class="nav-icon fas fa-user"></i>
               <p>Update Profile</p>
             </router-link>
-          </li>     
+          </li>
 
           <li class="nav-item">
-            <a href="javascript:void(0);" 
-               @click.prevent="signOutAccount" 
-               title="SignOut Your Account" 
-               class="nav-link">
+            <a
+              href="javascript:void(0);"
+              @click.prevent="signOutAccount"
+              title="SignOut Your Account"
+              class="nav-link"
+            >
               <i class="nav-icon fas fa-sign-out-alt"></i>
               <p>Sign Out</p>
             </a>
-          </li>   
-
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
@@ -134,53 +182,56 @@
 
 <script>
 export default {
-  name: 'Sidebar',
-  data : function(){
+  name: "Sidebar",
+  data: function () {
     return {
-      userProfile : {}
-    }
+      userProfile: {},
+    };
   },
-  methods :{
-    getProfile(){
+  methods: {
+    getProfile() {
       let _this = this;
-      _this.$store.dispatch('loggedProfile')
-      .then(function(result){
-         _this.userProfile = result;
-       })  
+      _this.$store.dispatch("loggedProfile").then(function (result) {
+        _this.userProfile = result;
+      });
     },
-    signOutAccount(){
-        
-        let _this = this; 
-       _this.$dialog.confirm('Are you sure to signout your account?')
-        .then(function(dialog) {
-          loader: true;  
-            //Delete Code start
-            _this.$loading.show();
-            _this.$store.dispatch("adminLogout",_this.loginData).then(res => { 
-                if( typeof(res.status)!='undefined' && (res.status == true) ) {  
-                  localStorage.removeItem('current_user');
-                  localStorage.removeItem('token');
-                  _this.$toastr.s("You have been logged out", "SUCCESS!!");
-                  setTimeout(function(){
-                    window.location.href='/admin/login';
-                  },500);
-                } else if( typeof(res.status)!='undefined' && (res.status == false) ){ 
-                  _this.$toastr.s("Error in logged out", "ERROR!!");
-                } 
-              })
-              .catch(e => {
-                _this.$toastr.s("Error in logged out", "ERROR!!"); 
-              }); 
+    signOutAccount() {
+      let _this = this;
+      _this.$dialog
+        .confirm("Are you sure to signout your account?")
+        .then(function (dialog) {
+          loader: true;
+          //Delete Code start
+          _this.$loading.show();
+          _this.$store
+            .dispatch("adminLogout", _this.loginData)
+            .then((res) => {
+              if (typeof res.status != "undefined" && res.status == true) {
+                localStorage.removeItem("current_user");
+                localStorage.removeItem("token");
+                _this.$toastr.s("You have been logged out", "SUCCESS!!");
+                setTimeout(function () {
+                  window.location.href = "/admin/login";
+                }, 500);
+              } else if (
+                typeof res.status != "undefined" &&
+                res.status == false
+              ) {
+                _this.$toastr.s("Error in logged out", "ERROR!!");
+              }
+            })
+            .catch((e) => {
+              _this.$toastr.s("Error in logged out", "ERROR!!");
+            });
         });
+    },
+  },
+  created() {
+    if (!!localStorage.getItem("token")) {
+      this.getProfile();
     }
   },
-  created(){
-    if( !!localStorage.getItem('token')) {
-       this.getProfile(); 
-    }
-  }   
-
-}
+};
 </script> 
-<style scoped> 
+<style scoped>
 </style>
--- resources/js/admin/router.js
+++ resources/js/admin/router.js
@@ -1,113 +1,268 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 import VueToastr from "vue-toastr";
-import config from '../config'
+import config from "../config";
 //Import All component Here
-import Dashboard from './components/protected/Dashboard';
+import Dashboard from "./components/protected/Dashboard";
 //Recipes
-import RecipesList from './components/protected/recipes/RecipesList';
-import RecipesForm from './components/protected/recipes/RecipesForm';
+import RecipesList from "./components/protected/recipes/RecipesList";
+import RecipesForm from "./components/protected/recipes/RecipesForm";
 //Recipe Categories
-import CategoriesList from './components/protected/categories/CategoriesList';
-import CategoryForm from './components/protected/categories/CategoryForm';
+import CategoriesList from "./components/protected/categories/CategoriesList";
+import CategoryForm from "./components/protected/categories/CategoryForm";
 //Recipe Tags
-import TagsList from './components/protected/tags/TagsList';
-import TagsForm from './components/protected/tags/TagsForm';
+import TagsList from "./components/protected/tags/TagsList";
+import TagsForm from "./components/protected/tags/TagsForm";
 //Blogs
-import BlogsList from './components/protected/blogs/BlogsList';
-import BlogsForm from './components/protected/blogs/BlogsForm';
+import BlogsList from "./components/protected/blogs/BlogsList";
+import BlogsForm from "./components/protected/blogs/BlogsForm";
 //Blog Categories
-import BlogCategoriesList from './components/protected/blog-categories/BlogCategoriesList';
-import BlogCategoryForm from './components/protected/blog-categories/BlogCategoryForm';
+import BlogCategoriesList from "./components/protected/blog-categories/BlogCategoriesList";
+import BlogCategoryForm from "./components/protected/blog-categories/BlogCategoryForm";
 //Blog Tags
-import BlogTagsList from './components/protected/blog-tags/BlogTagsList';
-import BlogTagsForm from './components/protected/blog-tags/BlogTagsForm';
+import BlogTagsList from "./components/protected/blog-tags/BlogTagsList";
+import BlogTagsForm from "./components/protected/blog-tags/BlogTagsForm";
 //Users
-import ClientsList from './components/protected/clients/ClientsList';
-import ClientsForm from './components/protected/clients/ClientsForm';
+import ClientsList from "./components/protected/clients/ClientsList";
+import ClientsForm from "./components/protected/clients/ClientsForm";
 //Authors
-import AuthorsList from './components/protected/authors/AuthorsList';
-import AuthorsForm from './components/protected/authors/AuthorsForm';
+import AuthorsList from "./components/protected/authors/AuthorsList";
+import AuthorsForm from "./components/protected/authors/AuthorsForm";
 //Auth
-import Login from './components/public/Login';
-import ForgotPassword from './components/public/ForgotPassword';
-import SetNewPassword from './components/public/SetNewPassword';
+import Login from "./components/public/Login";
+import ForgotPassword from "./components/public/ForgotPassword";
+import SetNewPassword from "./components/public/SetNewPassword";
 //Admin Profile
-import UpdateProfile from './components/protected/user-profile/UpdateProfile';
-import ChangePassword from './components/protected/user-profile/ChangePassword';
+import UpdateProfile from "./components/protected/user-profile/UpdateProfile";
+import ChangePassword from "./components/protected/user-profile/ChangePassword";
 
 //alert(config.URL_PREFIX_ADMIN)
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 const router = new VueRouter({
     base: config.URL_PREFIX_ADMIN,
     mode: "history",
     linkExactActiveClass: "active",
     routes: [
         //Auth Routes
-        { name: "login-admin", path: "/login", component: Login, meta: { visitorURL: true } },
-        { name: "forgot-password-admin", path: "/forgot-password", component: ForgotPassword, meta: { visitorURL: true } },
-        { name: "forgot-password-new", path: "/set-new-password", component: SetNewPassword, meta: { visitorURL: true } },
+        {
+            name: "login-admin",
+            path: "/login",
+            component: Login,
+            meta: { visitorURL: true },
+        },
+        {
+            name: "forgot-password-admin",
+            path: "/forgot-password",
+            component: ForgotPassword,
+            meta: { visitorURL: true },
+        },
+        {
+            name: "forgot-password-new",
+            path: "/set-new-password",
+            component: SetNewPassword,
+            meta: { visitorURL: true },
+        },
         //Profile Routes
-        { name: "update-profile", path: "/update-profile", component: UpdateProfile, meta: { protectedURL: true } },
-        { name: "change-password", path: "/change-password", component: ChangePassword, meta: { protectedURL: true } },
+        {
+            name: "update-profile",
+            path: "/update-profile",
+            component: UpdateProfile,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "change-password",
+            path: "/change-password",
+            component: ChangePassword,
+            meta: { protectedURL: true },
+        },
         //Dashboard Routes
-        { name: "index", path: "/", redirect: "/dashboard", meta: { protectedURL: true } },
-        { name: "dashboard", path: "/dashboard", component: Dashboard, meta: { protectedURL: true } },
+        {
+            name: "index",
+            path: "/",
+            redirect: "/dashboard",
+            meta: { protectedURL: true },
+        },
+        {
+            name: "dashboard",
+            path: "/dashboard",
+            component: Dashboard,
+            meta: { protectedURL: true },
+        },
         //Reipes Routes
-        { name: "recipes-list", path: "/recipes", component: RecipesList, meta: { protectedURL: true } },
-        { name: "edit-recipe", path: "/edit-recipe/:id", component: RecipesForm, meta: { protectedURL: true } },
-        { name: "add-recipe", path: "/add-recipe", component: RecipesForm, meta: { protectedURL: true } },
+        {
+            name: "recipes-list",
+            path: "/recipes",
+            component: RecipesList,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "edit-recipe",
+            path: "/edit-recipe/:id",
+            component: RecipesForm,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "add-recipe",
+            path: "/add-recipe",
+            component: RecipesForm,
+            meta: { protectedURL: true },
+        },
         //Recipe Categories Routes
-        { name: "categories-list", path: "/categories", component: CategoriesList, meta: { protectedURL: true } },
-        { name: "edit-category", path: "/edit-category/:id", component: CategoryForm, meta: { protectedURL: true } },
-        { name: "add-category", path: "/add-category", component: CategoryForm, meta: { protectedURL: true } },
+        {
+            name: "categories-list",
+            path: "/categories",
+            component: CategoriesList,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "edit-category",
+            path: "/edit-category/:id",
+            component: CategoryForm,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "add-category",
+            path: "/add-category",
+            component: CategoryForm,
+            meta: { protectedURL: true },
+        },
         //Recipes Tags Routes
-        { name: "tags-list", path: "/tags", component: TagsList, meta: { protectedURL: true } },
-        { name: "edit-tag", path: "/edit-tag/:id", component: TagsForm, meta: { protectedURL: true } },
-        { name: "add-tag", path: "/add-tag", component: TagsForm, meta: { protectedURL: true } },
+        {
+            name: "tags-list",
+            path: "/tags",
+            component: TagsList,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "edit-tag",
+            path: "/edit-tag/:id",
+            component: TagsForm,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "add-tag",
+            path: "/add-tag",
+            component: TagsForm,
+            meta: { protectedURL: true },
+        },
         //Blogs Routes
-        { name: "blogs-list", path: "/blogs", component: BlogsList, meta: { protectedURL: true } },
-        { name: "edit-blog", path: "/edit-blog/:id", component: BlogsForm, meta: { protectedURL: true } },
-        { name: "add-blog", path: "/add-blog", component: BlogsForm, meta: { protectedURL: true } },
+        {
+            name: "blogs-list",
+            path: "/blogs",
+            component: BlogsList,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "edit-blog",
+            path: "/edit-blog/:id",
+            component: BlogsForm,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "add-blog",
+            path: "/add-blog",
+            component: BlogsForm,
+            meta: { protectedURL: true },
+        },
         //Blog Categories Routes
-        { name: "blog-categories-list", path: "/blog-categories", component: BlogCategoriesList, meta: { protectedURL: true } },
-        { name: "blog-edit-category", path: "/edit-blog-category/:id", component: BlogCategoryForm, meta: { protectedURL: true } },
-        { name: "blog-add-category", path: "/add-blog-category", component: BlogCategoryForm, meta: { protectedURL: true } },
+        {
+            name: "blog-categories-list",
+            path: "/blog-categories",
+            component: BlogCategoriesList,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "edit-blog-category",
+            path: "/edit-blog-category/:id",
+            component: BlogCategoryForm,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "add-blog-category",
+            path: "/add-blog-category",
+            component: BlogCategoryForm,
+            meta: { protectedURL: true },
+        },
         //Blog Tags Routes
-        { name: "blog-tags-list", path: "/blog-tags", component: BlogTagsList, meta: { protectedURL: true } },
-        { name: "blog-edit-tag", path: "/edit-blog-tag/:id", component: BlogTagsForm, meta: { protectedURL: true } },
-        { name: "blog-add-tag", path: "/add-blog-tag", component: BlogTagsForm, meta: { protectedURL: true } },
+        {
+            name: "blog-tags-list",
+            path: "/blog-tags",
+            component: BlogTagsList,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "edit-blog-tag",
+            path: "/edit-blog-tag/:id",
+            component: BlogTagsForm,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "add-blog-tag",
+            path: "/add-blog-tag",
+            component: BlogTagsForm,
+            meta: { protectedURL: true },
+        },
         //Users Routes
-        { name: "clients-list", path: "/clients", component: ClientsList, meta: { protectedURL: true } },
-        { name: "edit-clients", path: "/edit-clients/:id", component: ClientsForm, meta: { protectedURL: true } },
-        { name: "add-clients", path: "/add-clients", component: ClientsForm, meta: { protectedURL: true } },
+        {
+            name: "clients-list",
+            path: "/clients",
+            component: ClientsList,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "edit-clients",
+            path: "/edit-clients/:id",
+            component: ClientsForm,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "add-clients",
+            path: "/add-clients",
+            component: ClientsForm,
+            meta: { protectedURL: true },
+        },
         //Authors Routes
-        { name: "authors-list", path: "/authors", component: AuthorsList, meta: { protectedURL: true } },
-        { name: "edit-authors", path: "/edit-authors/:id", component: AuthorsForm, meta: { protectedURL: true } },
-        { name: "add-authors", path: "/add-authors", component: AuthorsForm, meta: { protectedURL: true } },
-        //404 no route redirect   
-        { name: "404", path: "**", redirect: 'dashboard' }
+        {
+            name: "authors-list",
+            path: "/authors",
+            component: AuthorsList,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "edit-authors",
+            path: "/edit-authors/:id",
+            component: AuthorsForm,
+            meta: { protectedURL: true },
+        },
+        {
+            name: "add-authors",
+            path: "/add-authors",
+            component: AuthorsForm,
+            meta: { protectedURL: true },
+        },
+        //404 no route redirect
+        { name: "404", path: "**", redirect: "dashboard" },
     ],
     scrollBehavior(to, from, savedPosition) {
-        return { x: 0, y: 0 }
-    }
+        return { x: 0, y: 0 };
+    },
 });
 
 router.beforeResolve((to, from, next) => {
-    if (to.matched.some(record => record.meta.protectedURL)) {
-        if (!(!!localStorage.getItem('token'))) {
-            window.location.href = config.BASE_URL_ADMIN + 'login';
+    if (to.matched.some((record) => record.meta.protectedURL)) {
+        if (!!!localStorage.getItem("token")) {
+            window.location.href = config.BASE_URL_ADMIN + "login";
         } else {
-            next()
+            next();
         }
-    } else if (to.matched.some(record => record.meta.visitorURL)) {
-        if ((!!localStorage.getItem('token'))) {
-            window.location.href = config.BASE_URL_ADMIN + 'dashboard';
+    } else if (to.matched.some((record) => record.meta.visitorURL)) {
+        if (!!localStorage.getItem("token")) {
+            window.location.href = config.BASE_URL_ADMIN + "dashboard";
         } else {
-            next()
+            next();
         }
     } else {
-        next() // make sure to always call next()!
+        next(); // make sure to always call next()!
     }
 });
 
